de3937bc7f254c2bafe47e9795457e669df059cc
estizeiger <estizeiger@gmail.com>
2024-04-01 22:21:03 +0300

    graph improvments

--- src/app/components/member/member-graph/member-graph.component.ts
+++ src/app/components/member/member-graph/member-graph.component.ts
@@ -19,25 +19,75 @@ export class MemberGraphComponent implements OnInit {
     }
   }
 
+  // renderGraph(): void {
+  //   const years = [2020, 2021, 2022, 2023, 2024]; // corona years
+  //   const ctx = document.getElementById('memberGraph') as HTMLCanvasElement;
+  //   new Chart(ctx, {
+  //     type: 'bar',
+  //     data: {
+  //       labels: years.map(year => year.toString()),
+  //       datasets: [{
+  //         label: 'Members with Positive Result',
+  //         data: this.memberCountsByYear,
+  //         backgroundColor: ' rgb(228, 231, 249)',
+  //         borderColor: ' rgb(237, 56, 108)',
+  //         borderWidth: 2
+  //       }]
+  //     },
+  //     options: {
+  //       scales: {
+  //         y: {
+  //           beginAtZero: true,
+  //           suggestedMin: 0,
+  //           ticks: {
+  //             stepSize: 1, // Ensure only integer values are displayed
+  //             precision: 0 // Display only integer values
+  //           }
+  //         }
+  //       }
+  //     }
+  //   });
+  // }
+
   renderGraph(): void {
-    const years = [2020, 2021, 2022, 2023, 2024]; // corona years
+    const years = Object.keys(this.memberCountsByYear).map(year => parseInt(year));
+    const memberCounts = Object.values(this.memberCountsByYear);
+    const data = memberCounts.map((count, index) => ({
+      x: years[index],
+      y: count
+    }));
     const ctx = document.getElementById('memberGraph') as HTMLCanvasElement;
     new Chart(ctx, {
-      type: 'bar',
+      type: 'line',
       data: {
         labels: years.map(year => year.toString()),
         datasets: [{
           label: 'Members with Positive Result',
-          data: this.memberCountsByYear,
-          backgroundColor: ' rgb(228, 231, 249)',
-          borderColor: ' rgb(233, 30, 99)',
-          borderWidth: 2
+          data: data,
+          borderColor: 'rgb(237, 56, 108)',
+          backgroundColor: 'rgba(237, 56, 108, 0.2)',
+          borderWidth: 1,
+          fill: true
         }]
       },
       options: {
         scales: {
+          x: {
+            type: 'linear',
+            display: true,
+            title: {
+              display: true,
+              text: 'Year'
+            }
+          },
           y: {
             beginAtZero: true,
+            type: 'linear',
+            display: true,
+            title: {
+              display: true,
+              text: 'Number of Members'
+            },
             suggestedMin: 0,
             ticks: {
               stepSize: 1, // Ensure only integer values are displayed
